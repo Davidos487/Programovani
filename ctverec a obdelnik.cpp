b51be676401c0ccecdc9683130fe35bca0c3c47c
Davidos487 <david.safranek@skolakrizik.cz>
2025-04-07 08:25:25 +0200

    Add files via upload

---
+++ ctverec a obdelnik.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  int x = -42;
+  double y = 6.34;
+
+  int ax = abs(x);
+  double ay = fabs(ay);
+
+  printf("|%d| = %d\n", x, ax);
+  printf("|%f| = %f\n", y, ay);
+
+  return 0;
+}
