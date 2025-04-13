50ec9208d8ffda3607699a166c9e8d237859a312
Davidos487 <david.safranek@skolakrizik.cz>
2025-04-13 12:28:42 +0200

    Update programovani.cpp

--- programovani.cpp
+++ programovani.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int postava;
+int main()
+{
+
+    cout << "Mozne postavy jsou> 1 - Paladin, 2 - Lovec, 3 - Warlock\n";
+    do  {
+      bool chyba;
+      cout << "vyber si za jakou postavu chces hrat (zadej cislo 1-3)? ";
+      cin >> postava;
+      chyba=(postava < 1 || postava > 3);
+      if  ( ! chyba ) { break; } else { cout << "Zadali jste spatnou hodnotu\n"; }
+    } while ( true );
+
+    return 0;
+}
 
